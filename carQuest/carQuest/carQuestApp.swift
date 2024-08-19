216bbe6b7797366a840b69c6fcbc8e59966161da
qusammy <125623237+qusammy@users.noreply.github.com>
2024-08-19 11:07:53 -0500

    Initial Commit

---
+++ carQuest/carQuest/carQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  carQuestApp.swift
+//  carQuest
+//
+//  Created by Maddy Quinn on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct carQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
