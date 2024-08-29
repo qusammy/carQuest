68beb9fb1857a534ab3cd563c0c0377267526168
qusammy <125623237+qusammy@users.noreply.github.com>
2024-08-29 11:09:27 -0500

    Home page UI, Sign in UI, icon assets added

---
+++ carQuest/carQuest/listingCreation.swift
@@ -0,0 +1,18 @@
+//
+//  listingCreation.swift
+//  carQuest
+//
+//  Created by Maddy Quinn on 8/29/24.
+//
+
+import SwiftUI
+
+struct listingCreation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listingCreation()
+}
