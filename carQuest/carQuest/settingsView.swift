acf99c2f10aa4542c05c06a6079d131ae281c4d9
qusammy <125623237+qusammy@users.noreply.github.com>
2024-09-11 10:18:51 -0500

    listingCreation updates, settingsView created

---
+++ carQuest/carQuest/settingsView.swift
@@ -0,0 +1,18 @@
+//
+//  settingsView.swift
+//  carQuest
+//
+//  Created by Maddy Quinn on 9/11/24.
+//
+
+import SwiftUI
+
+struct settingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    settingsView()
+}
