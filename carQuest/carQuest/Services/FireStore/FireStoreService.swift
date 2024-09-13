db884ce14e18ee70a7055a963988ce954cd3e0b6
Youcefq3 <youcef07307@gmail.com>
2024-09-13 11:21:56 -0500

    project Structure

---
+++ carQuest/carQuest/Services/FireStore/FireStoreService.swift
@@ -0,0 +1,8 @@
+//
+//  FireStore.swift
+//  carQuest
+//
+//  Created by beraoud_981215 on 9/13/24.
+//
+
+import Foundation
---
+++ carQuest/carQuest/View/Views/RideDetails/CarDetails.swift
@@ -0,0 +1,18 @@
+//
+//  CarDetails.swift
+//  carQuest
+//
+//  Created by beraoud_981215 on 9/13/24.
+//
+
+import SwiftUI
+
+struct CarDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CarDetails()
+}
