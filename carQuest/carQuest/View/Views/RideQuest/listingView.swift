e414d1784038f95c854210710592ffa735363bea
qusammy <125623237+qusammy@users.noreply.github.com>
2024-09-26 08:36:24 -0500

    listingView creation and added photos to listingCreation

---
+++ carQuest/carQuest/View/Views/RideQuest/listingView.swift
@@ -0,0 +1,18 @@
+//
+//  listingView.swift
+//  carQuest
+//
+//  Created by Maddy Quinn on 9/24/24.
+//
+
+import SwiftUI
+
+struct listingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    listingView()
+}
