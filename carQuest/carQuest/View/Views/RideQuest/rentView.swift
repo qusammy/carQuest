1148e6efb63489c7179c720ff0d2eac364eb22c5
qusammy <125623237+qusammy@users.noreply.github.com>
2024-09-30 11:31:11 -0500

    basic FireStore document creation added. rentView added

---
+++ carQuest/carQuest/View/Views/RideQuest/rentView.swift
@@ -0,0 +1,18 @@
+//
+//  rentView.swift
+//  carQuest
+//
+//  Created by Maddy Quinn on 9/30/24.
+//
+
+import SwiftUI
+
+struct rentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    rentView()
+}
