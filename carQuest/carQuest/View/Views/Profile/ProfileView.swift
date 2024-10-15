3062b99535ca778f1277af344dbc2f671013901c
jhol0714 <james@benhollander.com>
2024-10-15 09:26:15 -0500

    n

--- carQuest/carQuest/View/Views/Profile/ProfileView.swift
+++ carQuest/carQuest/View/Views/Profile/ProfileView.swift
@@ -40,75 +40,75 @@ struct ProfileView: View {
                     List{
                         
                         //settings go here e.g. dark mode
-                        if showLogOut == true {
+                        if showSignInView == true {
                             NavigationLink(destination: UserProfileView(showSignInView: .constant(false))){
                                 Text("Profile")
                                     .font(.custom("Jost-Regular", size:20))
                                     .foregroundColor(.black)
-                                }
+                            }
                             Text("App Appearance")
                                 .font(.custom("Jost-Regular", size:20))
                                 .foregroundColor(.black)
                             Text("Privacy")
                                 .font(.custom("Jost-Regular", size:20))
                                 .foregroundColor(.black)
-                    List {
-                        
-                        if showSignInView == false {
+                        }
+                        List {
                             
-                            if let user = viewModel.user {
-                                Text("UserId: \(user.userId)")
-                            }
-                            
-                            Button("Log Out") {
-                                Task {
-                                    do {
-                                        try viewModel.signOut()
-                                        showSignInView = true
-                                    }catch {
-                                        print(error)
-                                    }
+                            if showSignInView == false {
+                                
+                                if let user = viewModel.user {
+                                    Text("UserId: \(user.userId)")
                                 }
-                            }
-                            Button {
-                                showingAlert = true
-                            }label: {
-                                Text("Delete Account")
-                                    .foregroundColor(.accentColor)
-                            }.alert("Are you sure you want to delete your account?", isPresented: $showingAlert) {
-                                Button(role: .destructive) {
+                                
+                                Button("Log Out") {
                                     Task {
                                         do {
-                                            try await viewModel.deleteAccount()
+                                            try viewModel.signOut()
                                             showSignInView = true
                                         }catch {
                                             print(error)
                                         }
                                     }
+                                }
+                                Button {
+                                    showingAlert = true
                                 }label: {
                                     Text("Delete Account")
-                                        .font(.custom("Jost-Regular", size: 25))
+                                        .foregroundColor(.accentColor)
+                                }.alert("Are you sure you want to delete your account?", isPresented: $showingAlert) {
+                                    Button(role: .destructive) {
+                                        Task {
+                                            do {
+                                                try await viewModel.deleteAccount()
+                                                showSignInView = true
+                                            }catch {
+                                                print(error)
+                                            }
+                                        }
+                                    }label: {
+                                        Text("Delete Account")
+                                            .font(.custom("Jost-Regular", size: 25))
+                                    }
+                                }message: {
+                                    Text("This action cannot be undone. \n The info on this account will be unrecoverable")
                                 }
-                            }message: {
-                                Text("This action cannot be undone. \n The info on this account will be unrecoverable")
                             }
+                        }.task {
+                            let currentUser = try? AuthenticationManager.shared.getAuthenticatedUser()
+                            self.showSignInView = currentUser == nil
+                            try? await viewModel.loadCurrentUser()
+                            
                         }
-                        }
-                }.onAppear {
+                        .foregroundColor(.accentColor)
+                        .background(Color.background)
+                        .scrollContentBackground(.hidden)
+                        .listRowBackground(Color(.background))
+                        bottomNavigationBar(showSignInView: .constant(false))
                     }
-                }.task {
-                    let currentUser = try? AuthenticationManager.shared.getAuthenticatedUser()
-                    self.showSignInView = currentUser == nil
-                    try? await viewModel.loadCurrentUser()
-                
+                    .padding()
                 }
-                .foregroundColor(.accentColor)
-                .background(Color.background)
-                .scrollContentBackground(.hidden)
-                .listRowBackground(Color(.background))
-                bottomNavigationBar(showSignInView: .constant(false))
             }
-            .padding()
         }
     }
 }
